628104b489f3e312b216bd3b0b8956477ccc6ddc
Jaidon Prakash <jaidonp23@gmail.com>
2026-01-29 23:46:45 +0000

    finished writing SVGWriter functions and tests and updating Readme.

--- README.md
+++ README.md
@@ -12,20 +12,22 @@ This the current status of the project is not done.
 * googleTest tutorial video - https://www.youtube.com/watch?v=x5_UxQ9wrH4&list=PL_dsdStdDXbo-zApdWB5XiF2aWpsqzV55&index=2
 * XML Escape chars - https://www.geeksforgeeks.org/javascript/how-to-escape-characters-in-xml/
 * constructor tutorial - https://www.geeksforgeeks.org/cpp/constructors-c/
-* 
-* 
-* 
-* 
-* 
+* svg attributes - https://developer.mozilla.org/en-US/docs/Web/SVG/Reference/Element/circle
+* svg information - https://www.w3.org/TR/SVG2/shapes.html
+* .gcno error - https://stackoverflow.com/questions/37978016/cmake-gcov-c-creating-wrong-gcno-files
+* unique ptr framework - https://www.fluentcpp.com/2017/09/22/make-pimpl-using-unique_ptr/
+* expat guide - https://www.xml.com/pub/1999/09/expat
+* getting xml data using parser - https://stackoverflow.com/questions/609376/geting-xml-data-using-xml-parser-expat
+* paths - https://developer.mozilla.org/en-US/docs/Web/SVG/Tutorials/SVG_from_scratch/Paths
+* path syntax - https://css-tricks.com/svg-path-syntax-illustrated-guide/
+* more googletest - https://github.com/google/googletest/blob/main/docs/advanced.md
+* code cov info - https://manpages.debian.org/unstable/lcov/genhtml.1.en.html
+* googeltest videos - https://www.youtube.com/playlist?list=PL5jc9xFGsL8GyES7nh-1yqljjdTvIFSsh
+* xml parser in c ex - https://www.youtube.com/watch?v=kPFYfTvMRs8
+* path video - https://www.youtube.com/watch?v=qdEU2V9UFmA
 
 
 ## Generative AI Use
 * explaining expat and how to use basic functions, summarizing the expat website given -https://chatgpt.com/share/696fcf80-368c-8009-8693-e8be21f221e8 
-* 
-* 
-* 
-* 
-* 
-
-
-### Response 1
+* expat example using an XML Parser - https://chatgpt.com/share/697bef46-de8c-8009-9835-fc433b5bd3c6
+* googletest ex - https://chatgpt.com/share/697bf067-6814-8009-b29f-7f47ac4f455d
--- src/SVGWriter.cpp
+++ src/SVGWriter.cpp
@@ -4,6 +4,11 @@
 #include <vector>
 #include <iostream>
 
+/*
+    CSVGWriter is a wrapper for C++ around the svg.c
+    holds unique ptr to SImplementation
+*/
+
 using std::cout;
 using std::endl;
 
@@ -11,19 +16,29 @@ struct CSVGWriter::SImplementation{
     std::shared_ptr< CDataSink > DSink;
     svg_context_ptr DContext;
 
+
+    //svg_create usies WriteFunction to write text and gets sent to CDataSink
     static svg_return_t WriteFunction(svg_user_context_ptr user, const char *text){
         SImplementation *Implementation = (SImplementation *)user;
+
         while(*text){
-            Implementation->DSink->Put(*text);
+            if(!Implementation->DSink->Put(*text)){
+                return SVG_ERR_IO;
+            }
             text++;
         }
         return SVG_OK;   
     }
 
+    static svg_return_t CleanupFunc(svg_user_context_ptr user){
+        (void)user;
+        return SVG_OK;
+    }
+
 
     SImplementation(std::shared_ptr< CDataSink > sink, TSVGPixel width, TSVGPixel height){
         DSink = sink;
-        DContext = svg_create(WriteFunction,nullptr,this,width,height);
+        DContext = svg_create(WriteFunction,CleanupFunc,this,width,height);
     }
 
     ~SImplementation(){
@@ -31,33 +46,131 @@ struct CSVGWriter::SImplementation{
     }
 
     std::string CreateStyleString(const TAttributes &style){
+        std::string styleString;
 
-        return "";
+        for(std::size_t i = 0; i < style.size(); i++){
+
+            styleString+= style[i].first;
+            styleString += ":";
+            styleString += style[i].second;
+
+            if(i+1 < style.size()){
+                styleString += "; ";
+
+            }
+
+        }
+
+        return styleString;
     }
 
+    //draws circle by calling svg_circle()
     bool Circle(const SSVGPoint &center, TSVGReal radius, const TAttributes &style){
+        //converst teh C++ SSVGPoint into C svg_point_t
         svg_point_t Center{center.DX,center.DY};
         std::string Style = CreateStyleString(style);
-        return svg_circle(DContext,&Center,radius,Style.c_str());
+        
+        svg_return_t Result = svg_circle(DContext, &Center, radius, Style.empty() ? nullptr : Style.c_str());
+        
+        return Result == SVG_OK;
     }
 
     bool Rectange(const SSVGPoint &topleft, const SSVGSize &size, const TAttributes &style){
+
+        //creating objects of the c struct
+        svg_point_t TopLeft{topleft.DX, topleft.DY};
+        svg_size_t Size{size.DWidth, size.DHeight};
+
+        //creates the style in text
+        std::string Style = CreateStyleString(style);
+
+        //if the style is empty, pass nullptr, if it has something then you pass that
+        svg_return_t Result = svg_rect(DContext, &TopLeft, &Size, Style.empty() ? nullptr : Style.c_str());
+
+        //true only if svg_circle returns svg_ok
+        return Result == SVG_OK;
+
 
     }
 
     bool Line(const SSVGPoint &start, const SSVGPoint &end, const TAttributes &style){
 
+        svg_point_t Start{start.DX, start.DY};
+        svg_point_t End{end.DX, end.DY};
+
+        std::string Style = CreateStyleString(style);
+
+        svg_return_t Result = svg_line(DContext, &Start, &End, Style.empty() ? nullptr : Style.c_str());
+        
+        return Result == SVG_OK;
+
+
     }
 
     bool SimplePath(const std::vector<SSVGPoint> points, const TAttributes &style){
+
+        if(points.empty()){
+            return false;
+        }
+
+        //build the attr string using path commands
+        std::string Path = "M ";
+        
+        char buffer[64];
+
+        //first points M * y
+        snprintf(buffer, sizeof(buffer), "%f %f", points[0].DX, points[0].DY);
+        Path+=buffer;
+
+        //the rest of the points
+        for(std::size_t i = 1; i < points.size(); i++){
+            Path+= " L ";
+            snprintf(buffer, sizeof(buffer), "%f %f", points[i].DX, points[i].DY);
+            Path+=buffer;
+        }
+
+        std::string Style = CreateStyleString(style);
+
+        //constructs the rest of the SVG element
+
+        std::string Output = "  <path d=\"";
+        Output += Path;
+        Output += "\"";
+
+        if(!Style.empty()){
+            Output += " style=\"";
+            Output += Style;
+            Output += "\"";
+        }
+
+        Output += "/>\n";
+
+        for(char c : Output){
+            if(!DSink->Put(c)){
+                return false;
+            }
+        }
+
+        return true;
 
     }
 
     bool GroupBegin(const TAttributes &attrs){
 
+        std::string AttrString = CreateStyleString(attrs);
+
+        svg_return_t Result = svg_group_begin(DContext, AttrString.empty() ? nullptr : AttrString.c_str());
+
+        return Result == SVG_OK;
+
+
     }
 
     bool GroupEnd(){
+
+        svg_return_t Result = svg_group_end(DContext);
+        return Result == SVG_OK;
+
 
     }
 
@@ -67,9 +180,7 @@ CSVGWriter::CSVGWriter(std::shared_ptr< CDataSink > sink, TSVGPixel width, TSVGP
     DImplementation = std::make_unique<SImplementation>(sink,width,height);
 }
 
-CSVGWriter::~CSVGWriter(){
-
-}
+CSVGWriter::~CSVGWriter() = default;
 
 bool CSVGWriter::Circle(const SSVGPoint &center, TSVGReal radius, const TAttributes &style){
     return DImplementation->Circle(center, radius,style);
@@ -80,17 +191,17 @@ bool CSVGWriter::Rectange(const SSVGPoint &topleft, const SSVGSize &size, const 
 }
 
 bool CSVGWriter::Line(const SSVGPoint &start, const SSVGPoint &end, const TAttributes &style){
-
+    return DImplementation->Line(start, end, style);    
 }
 
 bool CSVGWriter::SimplePath(const std::vector<SSVGPoint> points, const TAttributes &style){
-
+    return DImplementation->SimplePath(points, style);  
 }
 
 bool CSVGWriter::GroupBegin(const TAttributes &attrs){
-
+    return DImplementation->GroupBegin(attrs);  
 }
 
 bool CSVGWriter::GroupEnd(){
-
+    return DImplementation->GroupEnd();  
 }
